44235242d83f314a524540a0117032a189de7fe9
Guy Luz <guyluz11@gmail.com>
2023-11-09 21:58:39 +0200

    Updated packages

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,14 @@ import 'dart:async';
 import 'package:camera/camera.dart';
 import 'package:cbj_smart_device_flutter/presentation/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:network_tools_flutter/network_tools_flutter.dart';
+import 'package:path_provider/path_provider.dart';
 
 Future<void> main() async {
   try {
     WidgetsFlutterBinding.ensureInitialized();
+    final appDocDirectory = await getApplicationDocumentsDirectory();
+    await configureNetworkTools(appDocDirectory.path, enableDebugging: true);
   } on CameraException catch (e) {
     print('${e.code} ${e.description}');
   }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,9 +6,13 @@ import FlutterMacOS
 import Foundation
 
 import file_selector_macos
-import wakelock_macos
+import package_info_plus
+import path_provider_foundation
+import wakelock_plus
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
-  WakelockMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockMacosPlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  WakelockPlusMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockPlusMacosPlugin"))
 }
