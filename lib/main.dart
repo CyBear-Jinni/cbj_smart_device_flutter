6ccfa0d59ed920bf930c4150e834f103959d89d3
Guy Luz <guyluz11@gmail.com>
2023-11-27 00:02:07 +0200

    Fixing some errors in the console

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:camera/camera.dart';
+import 'package:cbj_smart_device_flutter/commands/flutter_commands.dart';
 import 'package:cbj_smart_device_flutter/presentation/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:network_tools_flutter/network_tools_flutter.dart';
@@ -9,6 +10,7 @@ import 'package:path_provider/path_provider.dart';
 Future<void> main() async {
   try {
     WidgetsFlutterBinding.ensureInitialized();
+    PhoneCommandsD();
     final appDocDirectory = await getApplicationDocumentsDirectory();
     await configureNetworkTools(appDocDirectory.path, enableDebugging: true);
   } on CameraException catch (e) {
